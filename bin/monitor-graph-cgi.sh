f9f9999ff08d4ebea49e0faa1030d45290547943
Nathaniel Wesley Filardo <nwfilardo@gmail.com>
2017-12-14 23:01:59 -0500

    Tweak web and rrdtool graph goo to separate DMX outs

--- bin/monitor-graph-cgi.sh
+++ bin/monitor-graph-cgi.sh
@@ -3,8 +3,8 @@
 DATA_DIR=/home/pi/sc/data
 OUTDIR=/home/pi/public_html/genimg
 
-DMX_SCALE=5
-DMX_SHIFT=20
+#DMX_SCALE=5
+#DMX_SHIFT=20
 
 TEMP_SCALE_UPPER=32
 TEMP_SCALE_LOWER=19
@@ -16,16 +16,25 @@ COMMON_ARGS=(
         --lazy
 )
 
+DMX_ARGS=(
+	--step=60 -v "DMX" --upper-limit 255 --lower-limit 0
+	--right-axis-label "DMX" --right-axis 1:0
+        DEF:dmxTN=${DATA_DIR}/tank-near-dmx.rrd:dmx:AVERAGE
+        DEF:dmxHN=${DATA_DIR}/hide-near-dmx.rrd:dmx:AVERAGE
+	LINE:dmxTN\#800000:"left side"
+	LINE:dmxHN\#008000:"left hide"
+)
+
 NEAR_ARGS=(
         --step=60 -v "degrees C" --upper-limit ${TEMP_SCALE_UPPER} --lower-limit ${TEMP_SCALE_LOWER} --rigid
-	--right-axis-label "DMX"
-	--right-axis ${DMX_SCALE}:-${DMX_SHIFT}
+	--right-axis-label "degrees C" --right-axis 1:0
+	# --right-axis-label "DMX"
+	# --right-axis ${DMX_SCALE}:-${DMX_SHIFT}
         DEF:tempH=${DATA_DIR}/heater-temp.rrd:temp:AVERAGE
         DEF:tempHN=${DATA_DIR}/hide-near-temp.rrd:temp:AVERAGE
-        DEF:dmxHN=${DATA_DIR}/hide-near-dmx.rrd:dmx:AVERAGE
         DEF:tempTN=${DATA_DIR}/tank-near-temp.rrd:temp:AVERAGE
-	CDEF:scaled_dmxHN=dmxHN,${DMX_SHIFT},+,${DMX_SCALE},/
-	LINE:scaled_dmxHN\#000000:"dmx left"
+	# CDEF:scaled_dmxHN=dmxHN,${DMX_SHIFT},+,${DMX_SCALE},/
+	# LINE:scaled_dmxHN\#000000:"dmx left"
         LINE2:tempH\#FF0000:"heater"
         LINE2:tempHN\#808000:"hide left"
         LINE2:tempTN\#00FF00:"tank left"
@@ -50,8 +59,7 @@ MID_ARGS=(
 
 FAR_ARGS=(
         --step=60 -v "degrees C" --upper-limit ${TEMP_SCALE_UPPER} --lower-limit ${TEMP_SCALE_LOWER} --rigid
-	--right-axis-label "degrees C"
-	--right-axis 1:0
+	--right-axis-label "degrees C" --right-axis 1:0
         DEF:tempHF=${DATA_DIR}/hide-far-temp.rrd:temp:AVERAGE
         DEF:tempTF=${DATA_DIR}/tank-far-temp.rrd:temp:AVERAGE
         LINE2:tempHF\#FF8000:"hide right"
@@ -61,15 +69,18 @@ FAR_ARGS=(
 
 OUTFILE="${QUERY_STRING}.png"
 case "$QUERY_STRING" in
-	near-4h)	PARAMS=(--start now-4h  --title "Melman Vivarium Near Sensor Data 4h"	${NEAR_ARGS[@]}) ;;
-	near-24h)	PARAMS=(--start now-24h --title "Melman Vivarium Near Sensor Data 24h"	${NEAR_ARGS[@]}) ;;
-	near-7d)	PARAMS=(--start now-7d  --title "Melman Vivarium Near Sensor Data 7d"	${NEAR_ARGS[@]}) ;;
+	dmx-4h)		PARAMS=(--start now-4h  --title "Melman Vivarium DMX Outputs 4h"	${DMX_ARGS[@]}) ;;
+	dmx-24h)	PARAMS=(--start now-24h --title "Melman Vivarium DMX Outputs 24h"	${DMX_ARGS[@]}) ;;
+	dmx-7d)		PARAMS=(--start now-7d  --title "Melman Vivarium DMX Outputs 7d"	${DMX_ARGS[@]}) ;;
+	near-4h)	PARAMS=(--start now-4h  --title "Melman Vivarium Left Sensor Data 4h"	${NEAR_ARGS[@]}) ;;
+	near-24h)	PARAMS=(--start now-24h --title "Melman Vivarium Left Sensor Data 24h"	${NEAR_ARGS[@]}) ;;
+	near-7d)	PARAMS=(--start now-7d  --title "Melman Vivarium Left Sensor Data 7d"	${NEAR_ARGS[@]}) ;;
 	mid-4h)		PARAMS=(--start now-4h  --title "Melman Vivarium Mid Sensor Data 4h"	${MID_ARGS[@]})  ;;
 	mid-24h)	PARAMS=(--start now-24h --title "Melman Vivarium Mid Sensor Data 24h"	${MID_ARGS[@]})  ;;
 	mid-7d)		PARAMS=(--start now-7d  --title "Melman Vivarium Mid Sensor Data 7d"	${MID_ARGS[@]})  ;;
-	far-4h)		PARAMS=(--start now-4h  --title "Melman Vivarium Far Sensor Data 4h"	${FAR_ARGS[@]})  ;;
-	far-24h)	PARAMS=(--start now-24h --title "Melman Vivarium Far Sensor Data 24h"	${FAR_ARGS[@]})  ;;
-	far-7d)		PARAMS=(--start now-7d  --title "Melman Vivarium Far Sensor Data 7d"	${FAR_ARGS[@]})  ;;
+	far-4h)		PARAMS=(--start now-4h  --title "Melman Vivarium Right Sensor Data 4h"	${FAR_ARGS[@]})  ;;
+	far-24h)	PARAMS=(--start now-24h --title "Melman Vivarium Right Sensor Data 24h"	${FAR_ARGS[@]})  ;;
+	far-7d)		PARAMS=(--start now-7d  --title "Melman Vivarium Right Sensor Data 7d"	${FAR_ARGS[@]})  ;;
 	*)
 		cat <<HERE
 Content-Type: text/html
